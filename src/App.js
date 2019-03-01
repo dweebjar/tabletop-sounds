772f79faf6492dd740f74b7d8d260557f0bd0d1b
Jake Crump <dweebjar@gmail.com>
2019-03-01 13:19:36 -0600

    Testing.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Test
+            Testing
           </p>
           <a
             className="App-link"
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Test
+            Testing
           </a>
         </header>
       </div>
